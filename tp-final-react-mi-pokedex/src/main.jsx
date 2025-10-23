dcf1b610e63649138c869f476647d1b862c79e59
Nicolas Olmedo <nicolasolmedo00@hotmail.com>
2025-10-23 20:33:02 -0300

    se crea rama pokedex

--- tp-final-react-mi-pokedex/src/main.jsx
+++ tp-final-react-mi-pokedex/src/main.jsx
@@ -1,29 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// 1. Importamos las herramientas de enrutamiento
+// enrutamiento
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// 2. Importamos los componentes de página que acabamos de crear
+// paginas
 import { HomePage } from './pages/HomePage';
 import { ListadoPage } from './pages/ListadoPage';
 import { DetallePage } from './pages/DetallePage';
 
-// Componente principal que define las rutas (App.jsx)
+// definicion de rutas
 function App() { 
   return (
     <BrowserRouter> 
-        {/* Aquí iría el componente reutilizable (ej. un Navbar) */}
-        <Routes> 
-            {/* Ruta principal (Home) */}
+        <Routes>  
             <Route path="/" element={<HomePage />} />
-            
-            {/* Ruta de Listado */}
             <Route path="/listado" element={<ListadoPage />} />
-            
-            {/* Ruta de Detalle (Ruta Dinámica con el parámetro :id) */}
             <Route path="/pokemon/:id" element={<DetallePage />} />
-
-            {/* Manejo de errores 404 */}
             <Route path="*" element={<h1>404 - Página no encontrada</h1>} /> 
         </Routes>
     </BrowserRouter>
