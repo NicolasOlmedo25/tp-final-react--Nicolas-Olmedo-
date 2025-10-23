e330599cf55c9a1fb2b509efbfcc6007b4b830e5
Nicolas Olmedo <nicolasolmedo00@hotmail.com>
2025-10-22 21:36:57 -0300

    instalacion de react vite y se agrego componente home

---
+++ tp-final-react-mi-pokedex/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+export function HomePage() {
+    return (
+        <main className="home-page-container">
+            <h1>Bienvenido a la NicoDex</h1>
+            <h2>Creada con React y Vite</h2>
+            <p>Esta aplicacion es sobre el mundo de los pokemon, aqui se encuentra la informacion sobre todos los pokemons, que lo disfrutes.</p>
+        </main>
+    );
+}        
